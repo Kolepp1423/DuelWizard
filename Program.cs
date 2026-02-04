6839a488d035656a0f2a373cd5298c243b3a3034
xinzz <alfinoarumi@gmail.com>
2026-02-04 21:12:27 +0700

    Menambahkan field + constructor + method

--- Program.cs
+++ Program.cs
@@ -2,5 +2,34 @@
 {
     class Wizard
     {
+        public string Name;
+        public int Energy;
+        public int Damage;
+
+        public Wizard(string name, int damage)
+        {
+            Name = name;
+            Energy = 100;
+            Damage = damage;
+        }
+
+        public void Attack(Wizard enemy)
+        {
+            enemy.Energy -= Damage;
+
+            if (enemy.Energy < 0)
+            {
+                enemy.Energy = 0;
+            }
+
+            Console.WriteLine($"{Name} menyerang {enemy.Name}");
+            Console.WriteLine($"Energi {enemy.Name} tersisa: {enemy.Energy}");
+            Console.WriteLine();
+        }
+
+        public void ShowStats()
+        {
+            Console.WriteLine($"Wizard: {Name} | Energy: {Energy}");
+        }
     }
 }
